461912b42f30d09744c08bd7d13e2bb0aab345a3
EnergyEdge <sanzhar.urmatbekov@gmail.com>
2025-02-13 23:23:54 +0600

    one more

--- image_extraction.py
+++
@@ -1,9 +0,0 @@
-import pytesseract
-from PIL import Image
-
-
-class ImageTextCLassifier:
-    def get_text(self, image):
-        image = Image.open(image)
-        extracted_text = pytesseract.image_to_string(image)
-        return extracted_text
