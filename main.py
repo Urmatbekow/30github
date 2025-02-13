35f61f136ca58f31ac0e839f7204841206d59fb5
EnergyEdge <sanzhar.urmatbekov@gmail.com>
2025-02-13 21:55:51 +0600

    yep

--- main.py
+++ main.py
@@ -8,12 +8,13 @@ model = client_model()
 base_beg_prompt = 'write me a prompt for image generation model, to draw an infographics with very good details like in an encyclopedia, you need to give the texts with intructions like pixel left right up and so on, about'
 
 #key topic prompt
-instruction_prompt = 'open ai history '
+instruction_prompt = ''
 
 #final instruction prompt
 base_end_prompt = '. consider the fact that image generation models can not really write texts well, can not draw the explanation. The last 10 symbols of your response should be a unique a hash out of this prompt(you can use any symbol)'
 
-response = model.create_slide(text =  base_beg_prompt + instruction_prompt + base_end_prompt)
+
+response = model.create_slide(text = 'write me prompt for image gen model to make pixel style night new york skyscrapers with a place to big square shape billboard' )#base_beg_prompt + instruction_prompt + base_end_prompt)
 slide_prompt = response.choices[0].message.content
 
 
@@ -21,4 +22,4 @@ print(slide_prompt)
 
 
 generator = TogetherImageGenerator(API_KEY)
-saved_path = generator.generate_image(prompt=slide_prompt[:-10], filename=f"{slide_prompt[-10:]}.png")
+saved_path = generator.generate_image(prompt=slide_prompt[:-10], filename=f"{slide_prompt[-10:-2]}.png")
