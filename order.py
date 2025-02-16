2b56cfc14ff502f985db96ed6257d0552d749e22
EnergyEdge <sanzhar.urmatbekov@gmail.com>
2025-02-16 21:51:41 +0600

    order_books

---
+++ order.py
@@ -0,0 +1,13 @@
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
+
+tokenizer = AutoTokenizer.from_pretrained("your-transformers-model")
+model_transformers = AutoModelForSequenceClassification.from_pretrained("your-transformers-model")
+
+def analyze_order_book(text_input):
+    inputs = tokenizer(text_input, return_tensors="pt", truncation=True)
+    outputs = model_transformers(**inputs)
+    return outputs
+
+order_book_data = "Example limit order book text data..."
+result = analyze_order_book(order_book_data)
+print(result)
